99d72eccc9d7c9ca7c8a440318ea36f58ad7edb9
Biom4st3r <Biom4st3r@gmail.com>
2022-01-17 01:25:51 -0600

    * Json creating EntityTypes and SmabTypes

--- src/main/java/adventurepack/mods/smab/ModInit.java
+++ src/main/java/adventurepack/mods/smab/ModInit.java
@@ -1,13 +1,39 @@
 package adventurepack.mods.smab;
 
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.List;
+
+import com.google.common.collect.Lists;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
+
+import adventurepack.mods.smab.smab.SmabBundle;
+import adventurepack.mods.smab.smab.SmabSpecies;
+import biom4st3r.libs.biow0rks.autojson.AutoJson;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.object.builder.v1.entity.FabricEntityTypeBuilder;
 
 public class ModInit implements ModInitializer
 {
 	public static final String MODID = "ap_smabs";
 
+	public static final List<SmabBundle> bundles = Lists.newArrayList();
+
 	@Override
 	public void onInitialize() {
-		
+		File[] files = new File("smabs").listFiles();
+		for (File file : files) {
+			try (FileReader stream = new FileReader(file)) {
+				JsonElement object = JsonParser.parseReader(stream);
+				SmabSpecies species = AutoJson.deserialize(SmabSpecies.class, object);
+				SmabBundle bundle = new SmabBundle(species, FabricEntityTypeBuilder.createMob());
+				bundles.add(bundle);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
 	}
 }
